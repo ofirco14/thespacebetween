2e94ef94993630fd41894d35278cc5d304788c62
Ofir Cohen <81193045+ofirco14@users.noreply.github.com>
2021-03-30 13:21:18 -0600

    Update Releases.js

--- src/components/Releases/Releases.js
+++ src/components/Releases/Releases.js
@@ -187,7 +187,7 @@ componentDidMount(){
      <div className="row center-align">
      <div className="col s12 m12 l12">
      <div className="copyrights">
-     Developed by <a href="https://ofirthedev.github.io/"  rel="noopener noreferrer" target="_blank" style={{color:'#b8ad82'}}>Ofir Cohen</a>
+     Developed by <a href="https://ofirco14.github.io/"  rel="noopener noreferrer" target="_blank" style={{color:'#b8ad82'}}>Ofir Cohen</a>
      </div>
      </div>
      </div>
